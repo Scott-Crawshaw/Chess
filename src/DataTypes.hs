5b75c1588818dea6b90d950736e18cc91b55ca09
Scott Crawshaw <scrawshaw@ambrahealth.com>
2021-08-02 15:18:11 -0400

    Parts 0 & 1 done

--- src/DataTypes.hs
+++ src/DataTypes.hs
@@ -11,3 +11,16 @@ data Type = King | Knight | Rook deriving Eq
 data PieceOnBoard = PieceOnBoard Piece Pos deriving Eq
 
 data GameState = GameState Color [PieceOnBoard]
+
+-- show instances
+instance Show Piece where
+    show (Piece King White) = "K"
+    show (Piece Knight White) = "N"
+    show (Piece Rook White) = "R"
+    show (Piece King Black) = "k"
+    show (Piece Knight Black) = "n"
+    show (Piece Rook Black) = "r"
+
+instance Show Color where
+    show Black = "B"
+    show White = "W"
